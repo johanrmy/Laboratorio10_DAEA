601bc511f47def7067d9b398e5aee3b2b4991fb9
Alumno <Alumno@LAB1504-18>
2024-05-25 15:59:41 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio10/Controllers/PedidosController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio10.Models;
+
+namespace Laboratorio10.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private NeptunoEntities db = new NeptunoEntities();
+
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            var pedidos = db.Pedidos.Where(p => p.Estado == true).Include(p => p.clientes).Include(p => p.compañiasdeenvios).Include(p => p.Empleados);
+            return View(pedidos.ToList());
+        }
+
+        // GET: Pedidos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = db.Pedidos.Find(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCliente = new SelectList(db.clientes, "idCliente", "NombreCompañia");
+            ViewBag.FormaEnvio = new SelectList(db.compañiasdeenvios, "idCompañiaEnvios", "nombreCompañia");
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "IdEmpleado", "Apellidos");
+            return View();
+        }
+
+        // POST: Pedidos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPedido,IdCliente,IdEmpleado,FechaPedido,FechaEntrega,FechaEnvio,FormaEnvio,Cargo,Destinatario,DireccionDestinatario,CiudadDestinatario,RegionDestinatario,CodPostalDestinatario,PaisDestinatario")] Pedidos pedidos)
+        {
+            if (ModelState.IsValid)
+            {
+                pedidos.Estado = true;
+                db.Pedidos.Add(pedidos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCliente = new SelectList(db.clientes, "idCliente", "NombreCompañia", pedidos.IdCliente);
+            ViewBag.FormaEnvio = new SelectList(db.compañiasdeenvios, "idCompañiaEnvios", "nombreCompañia", pedidos.FormaEnvio);
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "IdEmpleado", "Apellidos", pedidos.IdEmpleado);
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = db.Pedidos.Find(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCliente = new SelectList(db.clientes, "idCliente", "NombreCompañia", pedidos.IdCliente);
+            ViewBag.FormaEnvio = new SelectList(db.compañiasdeenvios, "idCompañiaEnvios", "nombreCompañia", pedidos.FormaEnvio);
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "IdEmpleado", "Apellidos", pedidos.IdEmpleado);
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPedido,IdCliente,IdEmpleado,FechaPedido,FechaEntrega,FechaEnvio,FormaEnvio,Cargo,Destinatario,DireccionDestinatario,CiudadDestinatario,RegionDestinatario,CodPostalDestinatario,PaisDestinatario")] Pedidos pedidos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedidos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCliente = new SelectList(db.clientes, "idCliente", "NombreCompañia", pedidos.IdCliente);
+            ViewBag.FormaEnvio = new SelectList(db.compañiasdeenvios, "idCompañiaEnvios", "nombreCompañia", pedidos.FormaEnvio);
+            ViewBag.IdEmpleado = new SelectList(db.Empleados, "IdEmpleado", "Apellidos", pedidos.IdEmpleado);
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = db.Pedidos.Find(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedidos pedidos = db.Pedidos.Find(id);
+            pedidos.Estado = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
